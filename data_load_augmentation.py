c3e4ac255eaeedd9a666a6631e0c1211de851f0a
ufris <jkood112@gmail.com>
2020-06-02 19:03:53 +0900

    Add files via upload

---
+++ data_load_augmentation.py
@@ -0,0 +1,100 @@
+import os, random
+import torch
+import torch.utils.data
+import torchvision
+import matplotlib.pyplot as plt
+from pycocotools.coco import COCO
+import cv2
+import numpy as np
+from skimage import io, transform
+from PIL import Image
+
+class myOwnDataset(torch.utils.data.Dataset):
+    def __init__(self, root, img_list, target,transforms=None):
+        self.root = root
+        self.transforms = transforms
+        self.img_list = img_list
+        self.target = target
+
+    def __getitem__(self, index):
+        # image = cv2.imread(os.path.join(self.root, self.img_list[index]))#, cv2.IMREAD_COLOR | cv2.IMREAD_IGNORE_ORIENTATION)
+
+        # pil_image = PIL.Image.open('Image.jpg').convert('RGB')
+        # open_cv_image = numpy.array(pil_image)
+        # # Convert RGB to BGR
+        # open_cv_image = open_cv_image[:, :, ::-1].copy()
+
+        # When using pytorch, must load image with pillow
+        image = Image.open(os.path.join(self.root, self.img_list[index]))
+        # Convert RGB to BGR
+        image = image.convert('RGB')
+        # Convert pillow to cv2
+        open_cv_image = np.array(image)
+        # plt.imshow(open_cv_image)
+        # plt.show()
+
+        image = open_cv_image[:, :, ::-1].copy()
+        # plt.imshow(image)
+        # plt.show()
+        # print(image.shape)
+
+
+        label = self.target[index]
+
+        # print('self.img_list[index]', self.img_list[index])
+        # print('self.target[index]', self.target[index])
+        clahe_do = random.randint(0,1)
+        if clahe_do == 1:
+            clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+            img1 = clahe.apply(image[:, :, 0])
+            img2 = clahe.apply(image[:, :, 1])
+            img3 = clahe.apply(image[:, :, 2])
+
+            image = np.stack([img1, img2, img3], axis=2)
+
+        image = cv2.resize(image, (224,224))
+
+        # label = torch.from_numpy(label)
+
+        if self.transforms is not None:
+            image = self.transforms(image)
+
+        return image, label
+
+    def __len__(self):
+        return len(self.img_list)
+
+class FaceLandmarksDataset(torch.utils.data.Dataset):
+    """Face Landmarks dataset."""
+
+    def __init__(self, csv_file, root_dir, transform=None):
+        """
+        Args:
+            csv_file (string): csv 파일의 경로
+            root_dir (string): 모든 이미지가 존재하는 디렉토리 경로
+            transform (callable, optional): 샘플에 적용될 Optional transform
+        """
+        self.landmarks_frame = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.landmarks_frame)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_name = os.path.join(self.root_dir,
+                                self.landmarks_frame.iloc[idx, 0])
+        image = io.imread(img_name)
+        landmarks = self.landmarks_frame.iloc[idx, 1:]
+        landmarks = np.array([landmarks])
+        landmarks = landmarks.astype('float').reshape(-1, 2)
+        sample = {'image': image, 'landmarks': landmarks}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
---
+++ pytorch_cls_tuto.py
@@ -0,0 +1,216 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import os, random, cv2
+import torch
+from torchvision import datasets, models, transforms
+import torchvision
+import torch.nn as nn
+from torch.nn import functional as F
+import torch.optim as optim
+from data_load_augmentation import myOwnDataset
+import torchsummary as summary
+
+input_path = "/media/crescom2/DATA/dataset/alien_vs_predator_thumbnails/data/"
+
+dog_path = '/media/crescom2/DATA/dataset/dog_cat/all_dog_cat/dog' + '/'
+cat_path = '/media/crescom2/DATA/dataset/dog_cat/all_dog_cat/cat' + '/'
+
+dog_list = ['dog/' + i for i in os.listdir(dog_path)]
+cat_list = ['cat/' + i for i in os.listdir(cat_path)]
+img_list = dog_list + cat_list
+
+total_cnt = len(img_list)
+train_cnt = int(total_cnt * 0.8)
+
+dog_target = [0 for i in range(len(dog_list))]
+cat_target = [1 for i in range(len(cat_list))]
+target_list = dog_target + cat_target
+
+random.seed(1)
+random.shuffle(img_list)
+random.seed(1)
+random.shuffle(target_list)
+
+print(img_list[:10])
+print(target_list[:10])
+
+train_img_list = img_list[:train_cnt]
+val_img_list = img_list[train_cnt:]
+
+train_target_list = target_list[:train_cnt]
+val_target_list = target_list[train_cnt:]
+
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+
+##### data load using ImageFolder
+
+# data_transforms = {
+#     'train':
+#     transforms.Compose([
+#         transforms.Resize((224,224)),
+#         transforms.RandomAffine(0, shear=10, scale=(0.8,1.2)),
+#         transforms.RandomHorizontalFlip(),
+#         transforms.ToTensor(),
+#         normalize
+#     ]),
+#     'validation':
+#     transforms.Compose([
+#         transforms.Resize((224,224)),
+#         transforms.ToTensor(),
+#         normalize
+#     ]),
+# }
+# image_datasets = {
+#     'train':
+#     datasets.ImageFolder(input_path + 'train', data_transforms['train']),
+#     'validation':
+#     datasets.ImageFolder(input_path + 'validation', data_transforms['validation'])
+# }
+#
+# dataloaders = {
+#     'train':
+#     torch.utils.data.DataLoader(image_datasets['train'],
+#                                 batch_size=32,
+#                                 shuffle=True,
+#                                 num_workers=0),  # for Kaggle
+#     'validation':
+#     torch.utils.data.DataLoader(image_datasets['validation'],
+#                                 batch_size=32,
+#                                 shuffle=False,
+#                                 num_workers=0)  # for Kaggle
+# }
+
+##### data load using own dataset
+
+data_transforms = {
+    'train':
+    transforms.Compose([
+        transforms.ToTensor(),
+        normalize
+    ]),
+    'validation':
+    transforms.Compose([
+        transforms.ToTensor(),
+        normalize
+    ]),
+}
+
+my_dataset = myOwnDataset(root='/media/crescom2/DATA/dataset/dog_cat/all_dog_cat',transforms=data_transforms['train'],img_list=train_img_list[:1000],target=train_target_list[:1000])
+val_my_dataset = myOwnDataset(root='/media/crescom2/DATA/dataset/dog_cat/all_dog_cat',transforms=data_transforms['validation'],img_list=val_img_list[:1000],target=val_target_list[:1000])
+
+dataloaders = {
+    'train':
+    torch.utils.data.DataLoader(my_dataset,
+                                batch_size=32,
+                                shuffle=True,
+                                num_workers=0),  # for Kaggle
+    'validation':
+    torch.utils.data.DataLoader(val_my_dataset,
+                                batch_size=32,
+                                shuffle=False,
+                                num_workers=0)  # for Kaggle
+}
+
+#### show img
+
+# def imshow(img):
+#     # img = img / 2 + 0.5     # unnormalize
+#     # npimg = img.numpy()
+#     # plt.imshow(np.transpose(npimg, (1, 2, 0)))
+#
+#     print('#######3',img.shape)
+#     img = img.numpy()
+#     img = np.transpose(img, (1, 2, 0))
+#     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#     plt.imshow(img)
+#     plt.show()
+
+# 학습용 이미지를 무작위로 가져오기
+# dataiter = iter(data_loader)
+# images = dataiter.next()
+
+# 이미지 보여주기
+# imshow(images[0])
+# 정답(label) 출력
+# print(' '.join('%5s' % classes[labels[j]] for j in range(1)))
+
+
+##### It don't use yet
+# inputs, masks = next(iter(dataloaders['train']))
+# print(inputs.shape, masks.shape)
+# plt.imshow(reverse_transform(inputs[3]))
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+model = models.resnet50(pretrained=True).to(device)
+box_model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+
+summary(box_model, input_size=(3, 224, 224))
+
+for param in model.parameters():
+    param.requires_grad = True
+
+print(model)
+
+model.fc = nn.Sequential(
+    nn.Linear(2048, 128),
+    nn.ReLU(inplace=True),
+    nn.Linear(128, 2),
+    ).to(device)
+
+print(model)
+
+prob = nn.Softmax()
+criterion = nn.CrossEntropyLoss().cuda()
+optimizer = optim.Adam(model.fc.parameters())
+
+# cnt_dict = {'train':len(train_img_list[:1000]), 'validation':len(val_img_list[:1000])}
+
+def train_model(model, criterion, optimizer, num_epochs=10):
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch+1, num_epochs))
+        print('-' * 10)
+
+        for phase in ['train','validation']:
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # print(inputs.shape)
+                # print(labels)
+
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+
+                if phase == 'train':
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+                _, preds = torch.max(outputs, 1)
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / 1000 #len(my_dataset)
+
+            epoch_acc = running_corrects.double() / 1000 # len(val_my_dataset)
+
+            print('{} loss: {:.4f}, acc: {:.4f}'.format(phase,
+                                                        epoch_loss,
+                                                        epoch_acc))
+    return model
+
+model_trained = train_model(model, criterion, optimizer, num_epochs=10)
+
+torch.save(model_trained.state_dict(), 'models/pytorch/weights.h5')
